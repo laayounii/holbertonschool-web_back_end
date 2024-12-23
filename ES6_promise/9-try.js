b2e7b254079ca2395d717a95a6f6c838c83351b5
laayounii <meedaziz001@gmail.com>
2024-12-23 15:46:42 +0100

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,11 @@
+const guardrail = (mathFunction) => {
+    const queue = [];
+    try {
+      queue.push(mathFunction(), 'Guardrail was processed');
+    } catch (error) {
+      queue.push(error.toString(), 'Guardrail was processed');
+    }
+    return queue;
+  };
+  
+  export default guardrail;
